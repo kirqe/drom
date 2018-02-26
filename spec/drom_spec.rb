cd18426500b91eabc183d3b676151d764d671002
kirqe <ikll7@yahoo.com>
2018-02-26 15:46:46 +0300

    rs

--- spec/drom_spec.rb
+++ spec/drom_spec.rb
@@ -1,17 +1,16 @@
 require "spec_helper"
 
 RSpec.describe Drom do
-  it "has a version number" do
-    expect(Drom::VERSION).not_to be nil
+  before :all do
+    @search = Drom.search(make: "mazda", model: "rx-8", maxprice: 100000)
+    @get_single = Drom.get_single_listing("https://rostov-na-donu.drom.ru/mazda/rx-8/26689748.html")
   end
 
-  it "tests #Drom.search() return type" do
-    res = Drom.search(make: "mazda", model: "rx-8", maxprice: 100000)
-    expect(res).to be_an_instance_of(Drom::Search)
+  it "tests #Drom.search()" do
+    expect(@search).to be_an_instance_of(Drom::Search)
   end
 
-  it "tests #Drom.search() return type" do
-    res = Drom.get_single_listing("https://rostov-na-donu.drom.ru/mazda/rx-8/26689748.html")
-    expect(res).to be_an_instance_of(Hash)
+  it "tests #Drom.get_single_listing()" do
+    expect(@get_single).to be_an_instance_of(Hash)
   end
 end
